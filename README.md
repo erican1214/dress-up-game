964f842048ad7caca12f03c5095690085b4d1a47
erican1214 <erican1214@outlook.com>
2025-10-07 12:42:42 -0400

    Initial commit of project, project is functional

---
+++ README.md
@@ -0,0 +1,51 @@
+# Dress-Up Game
+## About
+This is a game where you can choose the outfit and customize the appearance of your character. Currently, there is only a female option for customization.
+
+## Important Information
+### URL to webpage (for those who don't want to download packages)
+
+### Applications Needed
+- Node.js
+- VSCode (or any IDE)
+- Google Chrome (or any browser)
+
+### Packages Needed
+- Express.js (The libraries and dependencies can be found in package-lock.json)
+
+- To install Express.js, navigate to the project directory, and run the following command prompts (for Windows, use npm.cmd instead of npm)
+    - npm init -y
+    - npm install express
+
+- More command prompts if necessary
+    - npm install ejs
+
+### How to run webpage (if you're using Node.js)
+- Navigate the project directory
+- Type "node server.js" if you are only interacting
+- Type "npm run dev" if you want to continuously edit this webpage (for Windows, use npm.cmd instead of npm)
+- Go on your browser and type "localhost:3000" in the search bar
+
+### Compatability/Accessibility
+- I have only tested this webpage on a Google Chrome Browser. It is possible that it won't be compatible with other browsers
+
+### Image Attributions
+
+## File Overview
+&larr; README.md
+- This file currently, shows details about how this webpage works
+
+&larr; index.ejs
+- HTML file that shows content of the webpage
+
+&larr; style.css
+- CSS file that customizes that content of the webpage
+
+&larr; script.js
+- Javascript file that adds interaction to the webpage and listens for events
+
+&larr; server.js
+- Javascript file that connects the code to server
+
+&larr; package.json, package-lock.json
+- Shows the necessary libraries/packages/dependencies for Node.js to run Express.js
---
+++ public/script.js
@@ -0,0 +1,256 @@
+const skinButton = document.getElementById("skinButton");
+const hairButton = document.getElementById("hairButton");
+const shirtButton = document.getElementById("shirtButton");
+const pantsButton = document.getElementById("pantsButton");
+const dressButton = document.getElementById("dressButton");
+const sockButton = document.getElementById("sockButton");
+const shoeButton = document.getElementById("shoeButton");
+let allSelections = document.getElementsByClassName("selection");
+
+let skinOptions = document.getElementsByClassName("skinOptions");
+let hairOptions = document.getElementsByClassName("hairOptions");
+let pantsOptions = document.getElementsByClassName("pantsOptions");
+let shirtOptions = document.getElementsByClassName("shirtOptions");
+let dressOptions = document.getElementsByClassName("dressOptions");
+let sockOptions = document.getElementsByClassName("sockOptions");
+let shoeOptions = document.getElementsByClassName("shoeOptions");
+
+
+const skinList =
+[
+    "assets/dress_up_elements/skins/skin_1.png",
+    "assets/dress_up_elements/skins/skin_2.png",
+    "assets/dress_up_elements/skins/skin_3.png",
+    "assets/dress_up_elements/skins/skin_4.png",
+    "assets/dress_up_elements/skins/skin_5.png",
+    "assets/dress_up_elements/skins/skin_6.png",
+    "assets/dress_up_elements/skins/skin_7.png",
+    "assets/dress_up_elements/skins/skin_8.png",
+    "assets/dress_up_elements/skins/skin_9.png",
+    "assets/dress_up_elements/skins/skin_10.png"
+];
+
+const hairList =
+[
+    "assets/dress_up_elements/hair/hair_1.png",
+    "assets/dress_up_elements/hair/hair_2.png",
+    "assets/dress_up_elements/hair/hair_3.png",
+    "assets/dress_up_elements/hair/hair_4.png",
+    "assets/dress_up_elements/hair/hair_5.png",
+    "assets/dress_up_elements/hair/hair_6.png",
+    "assets/dress_up_elements/hair/hair_7.png",
+    "assets/dress_up_elements/hair/hair_8.png",
+    "assets/dress_up_elements/hair/hair_9.png",
+    "assets/dress_up_elements/front_hair/front_hair_1.png",
+    "assets/dress_up_elements/front_hair/front_hair_2.png"
+];
+
+let shirtList =
+[
+    "assets/dress_up_elements/shirts/shirt_1.png",
+    "assets/dress_up_elements/shirts/shirt_2.png",
+    "assets/dress_up_elements/shirts/shirt_3.png",
+    "assets/dress_up_elements/shirts/shirt_4.png",
+    "assets/dress_up_elements/shirts/shirt_5.png",
+    "assets/dress_up_elements/shirts/shirt_6.png",
+    "assets/dress_up_elements/shirts/shirt_7.png",
+    "assets/dress_up_elements/shirts/shirt_8.png"
+];
+
+let pantsList =
+[
+    "assets/dress_up_elements/pants/pants_1.png",
+    "assets/dress_up_elements/pants/pants_2.png",
+    "assets/dress_up_elements/pants/pants_3.png",
+    "assets/dress_up_elements/pants/pants_4.png",
+    "assets/dress_up_elements/pants/pants_5.png",
+    "assets/dress_up_elements/pants/pants_6.png",
+    "assets/dress_up_elements/pants/pants_7.png",
+    "assets/dress_up_elements/pants/pants_8.png",
+    "assets/dress_up_elements/pants/pants_9.png",
+    "assets/dress_up_elements/pants/pants_10.png",
+    "assets/dress_up_elements/pants/pants_11.png"
+];
+
+let dressList =
+[
+    "assets/dress_up_elements/dresses/dress_1.png",
+    "assets/dress_up_elements/dresses/dress_2.png",
+    "assets/dress_up_elements/dresses/dress_3.png",
+    "assets/dress_up_elements/dresses/dress_4.png",
+    "assets/dress_up_elements/dresses/dress_5.png",
+    "assets/dress_up_elements/dresses/dress_6.png",
+    "assets/dress_up_elements/dresses/dress_7.png",
+    "assets/dress_up_elements/dresses/dress_8.png",
+    "assets/dress_up_elements/dresses/dress_9.png"
+];
+
+let sockList =
+[
+    "assets/dress_up_elements/socks/socks_1.png",
+    "assets/dress_up_elements/socks/socks_2.png",
+    "assets/dress_up_elements/socks/socks_3.png",
+    "assets/dress_up_elements/socks/socks_4.png",
+    "assets/dress_up_elements/socks/socks_5.png",
+    "assets/dress_up_elements/socks/socks_6.png"
+];
+
+let shoeList =
+[
+    "assets/dress_up_elements/shoes/shoes_1.png",
+    "assets/dress_up_elements/shoes/shoes_2.png",
+    "assets/dress_up_elements/shoes/shoes_3.png",
+    "assets/dress_up_elements/shoes/shoes_4.png",
+    "assets/dress_up_elements/shoes/shoes_5.png",
+    "assets/dress_up_elements/shoes/shoes_6.png",
+    "assets/dress_up_elements/shoes/shoes_7.png"
+];
+
+let mainBody = document.getElementById("mainBody");
+let hair = document.getElementById("hair");
+let pants = document.getElementById("pants");
+let shirt = document.getElementById("shirt");
+let dress = document.getElementById("dress");
+let frontHair = document.getElementById("frontHair");
+let socks = document.getElementById("socks");
+let shoes = document.getElementById("shoes");
+
+function displayCurrentSelection(selection) {
+    for (let i = 0; i < allSelections.length; i++) {
+        if (allSelections[i].getAttribute('id') === selection) {
+            allSelections[i].style.display = "flex";
+        }
+        else {
+            allSelections[i].style.display = "none";
+            allSelections[i].getAttribute('id');
+        }
+    }
+}
+
+if (skinButton) {
+    skinButton.addEventListener("click", event => {
+        displayCurrentSelection("skinSelection")
+    });
+}
+
+if (hairButton) {
+    hairButton.addEventListener("click", event => {
+        displayCurrentSelection("hairSelection")
+    });
+}
+
+if (shirtButton) {
+    shirtButton.addEventListener("click", event => {
+        displayCurrentSelection("shirtSelection")
+    });
+}
+
+if (pantsButton) {
+    pantsButton.addEventListener("click", event => {
+        displayCurrentSelection("pantsSelection")
+    });
+}
+
+if (dressButton) {
+    dressButton.addEventListener("click", event => {
+        displayCurrentSelection("dressSelection")
+    });
+}
+
+if (sockButton) {
+    sockButton.addEventListener("click", event => {
+        displayCurrentSelection("sockSelection")
+    });
+}
+
+if (shoeButton) {
+    shoeButton.addEventListener("click", event => {
+        displayCurrentSelection("shoeSelection")
+    });
+}
+
+if (skinOptions) {
+    for (let i = 0; i < skinOptions.length; i++) {
+        skinOptions[i].addEventListener("click", event => {
+            mainBody.src = skinList[i];
+        })
+    }
+}
+
+if (hairOptions) {
+    for (let i = 0; i < hairOptions.length; i++) {
+        hairOptions[i].addEventListener("click", event => {
+            if (i >= 9) {
+                hair.style.display = "none";
+                frontHair.style.display = "block";
+                frontHair.src = hairList[i];
+            }
+            else if (i == 7) {
+                hair.style.display = "block";
+                hair.src = hairList[i];
+                frontHair.style.display = "block";
+                frontHair.src = "assets/dress_up_elements/front_hair/bangs_8.png";
+            }
+            else if (i == 8) {
+                hair.style.display = "block";
+                hair.src = hairList[i];
+                frontHair.style.display = "block";
+                frontHair.src = "assets/dress_up_elements/front_hair/bangs_9.png";
+            }
+            else {
+                frontHair.style.display = "none";
+                hair.style.display = "block";
+                hair.src = hairList[i];
+            }
+        })
+    }
+}
+
+if (pantsOptions) {
+    for (let i = 0; i < pantsOptions.length; i++) {
+        pantsOptions[i].addEventListener("click", event => {
+            dress.style.display = "none";
+            pants.style.display = "block";
+            pants.src = pantsList[i];
+        })
+    }
+}
+
+if (shirtOptions) {
+    for (let i = 0; i < shirtOptions.length; i++) {
+        shirtOptions[i].addEventListener("click", event => {
+            dress.style.display = "none";
+            shirt.style.display = "block";
+            shirt.src = shirtList[i];
+        })
+    }
+}
+
+if (dressOptions) {
+    for (let i = 0; i < dressOptions.length; i++) {
+        dressOptions[i].addEventListener("click", event => {
+            pants.style.display = "none";
+            shirt.style.display = "none";
+            dress.style.display = "block";
+            dress.src = dressList[i];
+        })
+    }
+}
+
+if (sockOptions) {
+    for (let i = 0; i < sockOptions.length; i++) {
+        sockOptions[i].addEventListener("click", event => {
+            socks.style.display = "block";
+            socks.src = sockList[i];
+        })
+    }
+}
+
+if (shoeOptions) {
+    for (let i = 0; i < shoeOptions.length; i++) {
+        shoeOptions[i].addEventListener("click", event => {
+            shoes.style.display = "block";
+            shoes.src = shoeList[i];
+        })
+    }
+}
